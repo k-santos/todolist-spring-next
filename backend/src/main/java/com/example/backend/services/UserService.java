f7473cd997692b70d5a02cf50cc28acdcab4bf96
Kleber Santos <ksantosdev@gmail.com>
2024-05-31 10:20:11 -0300

    feat: encrypt password

--- backend/src/main/java/com/example/backend/services/UserService.java
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -2,23 +2,22 @@ package com.example.backend.services;
 
 import com.example.backend.entities.User;
 import com.example.backend.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User createUser(User user) {
-        logger.info("Creating user: {}", user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = userRepository.save(user);
-        logger.info("User saved: {}", savedUser);
         return savedUser;
     }
 }
